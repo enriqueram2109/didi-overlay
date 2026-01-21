05867a90a3ba6991172c8a132c13c26cd2522cc8
enriqueram2109 <enriqueram2109@gmail.com>
2026-01-21 10:45:53 -0600

    Create MainActivity for overlay permission handling
    Implement MainActivity to manage overlay permissions.

---
+++ app/app/src/main/app/src/main/java/com/didioverlay/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.didioverlay
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        } else {
+            startService(Intent(this, OverlayService::class.java))
+        }
+
+        finish()
+    }
+}
