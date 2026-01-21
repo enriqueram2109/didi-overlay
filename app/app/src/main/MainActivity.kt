5a868f4a5c21da001dca3369f620ac6331c6a718
enriqueram2109 <enriqueram2109@gmail.com>
2026-01-21 12:06:57 -0600

    Add MainActivity to request overlay permission

---
+++ app/app/src/main/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.didioverlay
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, 1234)
+        } else {
+            startOverlay()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1234) {
+            if (Settings.canDrawOverlays(this)) {
+                startOverlay()
+            }
+        }
+    }
+
+    private fun startOverlay() {
+        val intent = Intent(this, OverlayService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+        finish()
+    }
+}
